470ccab8cf35c9253dc08cb969550e6e9c45004d
Piero Proietti <piero.proietti@gmail.com>
2023-12-26 14:09:32 +0100

    cercansi QT terminal

--- mainwindow.py
+++ mainwindow.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import subprocess
 
 # Import QtWidgets, QtCore
 from PySide6 import QtCore, QtWidgets
@@ -27,6 +28,18 @@ class MyMainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             button.clicked.connect(self.exit)
             self.setCentralWidget(button)
 
+        # check /etc/penguins-eggs.d/eggs.yaml
+        file_eggs='/etc/penguins-eggs.d/eggs.yaml'
+        dirname_eggs=os.path.dirname(file_eggs)
+        # basename_eggs=os.path.basename(file_eggs)
+        if os.path.exists(dirname_eggs):
+            if not os.path.isfile(file_eggs):
+                try:
+                    subprocess.run(['/usr/bin/eggs', 'dad', '--default'], check=True)
+                except subprocess.CalledProcessError as e:
+                    print(f'Command {e.cmd} failed with error {e.returncode}')
+                print('eggs dad --default')
+                      
         # Signals are emitted by objects 
         self.action_About.triggered.connect(self.about)
         self.action_Configure.triggered.connect(self.configure)
@@ -45,7 +58,6 @@ class MyMainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         # in init prima di show, inizializziamo tutto
         self.show()
 
-
     ##
     #
     @QtCore.Slot()
@@ -56,10 +68,9 @@ class MyMainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     #
     @QtCore.Slot()
     def configure(self):
-        self.form.show()
-        #print ("Configure")
-
-
+        # visualizza form
+        pass
+        
     ##
     #
     @QtCore.Slot()
@@ -71,31 +82,53 @@ class MyMainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     #
     @QtCore.Slot()
     def kill(self):
-        print ("Kill")
+        try:
+            subprocess.run(['/usr/bin/eggs', 'kill'], check=True)
+        except subprocess.CalledProcessError as e:
+            print(f'Command {e.cmd} failed with error {e.returncode}')
+
+        #result=subprocess.run(['/usr/bin/eggs', 'kill', '--nointeractive'], stdout=subprocess.PIPE)
+        #print(result.stdout.decode())
 
     ##
     #
     @QtCore.Slot()
     def produce(self):
-        print ("Produce")
+        try:
+            subprocess.run(['/usr/bin/eggs', 'produce'], check=True)
+        except subprocess.CalledProcessError as e:
+            print(f'Command {e.cmd} failed with error {e.returncode}')
+        print ("Produce end")
 
     ##
     #
     @QtCore.Slot()
     def clean(self):
-        print ("Clean")
+        try:
+            subprocess.run(['/usr/bin/eggs', 'tools', 'clean'], check=True)
+        except subprocess.CalledProcessError as e:
+            print(f'Command {e.cmd} failed with error {e.returncode}')
+        print ("Clean end")
 
     ##
     #
     @QtCore.Slot()
     def Ppa(self):
-        print ("PPA")
+        try:
+            subprocess.run(['/usr/bin/eggs', 'tools', 'ppa'], check=True)
+        except subprocess.CalledProcessError as e:
+            print(f'Command {e.cmd} failed with error {e.returncode}')
+        print ("PPA end")
 
     ##
     #
     @QtCore.Slot()
     def Skel(self):
-        print ("Skel")
+        try:
+            subprocess.run(['/usr/bin/eggs', 'tools', 'skel'], check=True)
+        except subprocess.CalledProcessError as e:
+            print(f'Command {e.cmd} failed with error {e.returncode}')
+        print ("Skel end")
 
     ##
     #
