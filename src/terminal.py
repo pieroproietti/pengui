21e7ff283d6ef27e552d287c5a4871be5823deca
Piero Proietti <piero.proietti@gmail.com>
2024-01-01 20:38:22 +0100

    dbus problem

--- src/terminal.py
+++ src/terminal.py
@@ -7,7 +7,6 @@ import os
 class Terminal(QProcess):
 
     def execute(self, command):
-        print("command:" + command)
         if os.geteuid() != 0:
             if command !='eggs wardrobe get':
                 command='sudo ' + command
@@ -15,6 +14,9 @@ class Terminal(QProcess):
         # process start
         process = QProcess(self)
         process.setProgram("/usr/bin/x-terminal-emulator")
+        # can not run without dbus-launch, so we need:
+        # sudo apt-get install dbus-x11
+        # sudo service dbus restart
         process.setArguments(["-e", command])
         process.start()
 
