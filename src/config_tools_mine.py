f8aadd16d96575316b7f17c2de0b188e3b8d4f89
Piero Proietti <piero.proietti@gmail.com>
2024-02-11 03:37:51 +0100

    rifatto config_tools con AI

--- src/config_tools_mine.py
+++ src/config_tools_mine.py
@@ -8,20 +8,24 @@ from ui.ui_config_tools import Ui_Dialog
 
 import os
 import yaml
-from utilies import U
+from  utilies import U
 
-class Config_Tools(QtWidgets.QDialog, Ui_Dialog):
+##
+#
+class Config_Tools(Ui_Dialog, QDialog):    
+
     def __init__(self, parent=None):
-        super().__init__(parent)
-        self.setupUi(self)
+        super().__init__(parent) # parent
+        self.setupUi(self) # mandatory
 
-        self.tools_yaml_path = os.path.join(U.conf_path, 'tools.yaml')
-        if not os.path.exists(self.tools_yaml_path):
-            msg_box = QMessageBox(self)
-            msg_box.setText(f"Can't find {self.tools_yaml_path}")
-            msg_box.exec()
-            return
-
+        self.tools_yaml_path=U.conf_path+'/tools.yaml'
+        if not os.path.isfile(self.tools_yaml_path):
+            msgBox = QMessageBox(self)
+            msgBox.setText("Can't find " + self.tools)
+            msgBox.exec()
+            quit()
+        
+        # now we read tools.yaml
         with open(self.tools_yaml_path, 'r') as file:
             tools = yaml.safe_load(file)
 
@@ -35,27 +39,34 @@ class Config_Tools(QtWidgets.QDialog, Ui_Dialog):
 
         self.pushButtonHelp.clicked.connect(self.help)
         self.pushButtonSave.clicked.connect(self.save)
-        self.pushButtonDiscard.clicked.connect(self.close)
+        self.pushButtonDiscard.clicked.connect(self.close_me)
 
-        self.buttonBox.accepted.connect(self.save)
-        self.buttonBox.rejected.connect(self.close)
-
+        self.buttonBox.accepted.connect(self.close_me)
+        self.buttonBox.rejected.connect(self.close_me)
+        
         self.show()
+
 
     def help(self):
         webbrowser.open('https://github.com/pieroproietti/penguins-eggs?tab=readme-ov-file#eggs-dad')
 
+    ##
+    #
+    @QtCore.Slot()
     def save(self):
-        tools = {
-            "localPathIso": self.lineEditLocalPathIso.text(),
-            "localPathDeb": self.lineEditLocalPathDeb.text(),
-            "remoteHost": self.lineEditRemoteHost.text(),
-            "remoteUser": self.lineEditRemoteUser.text(),
-            "remotePathIso": self.lineEditRemotePathIso.text(),
-            "remotePathDeb": self.lineEditRemotePathDeb.text(),
-            "filterDeb": self.lineEditFilterDeb.text(),
-        }
+        with open(self.tools_yaml_path, 'r') as file:
+            tools = yaml.safe_load(file)
 
+        tools["localPathIso"]=self.lineEditLocalPathIso.text()
+        tools["localPathDeb"]=self.lineEditLocalPathDeb.text()
+        tools["remoteHost"]=self.lineEditRemoteHost.text()
+        tools["remoteUser"]=self.lineEditRemoteUser.text()
+        tools["remotePathIso"]=self.lineEditRemotePathIso.text()
+        tools["remotePathDeb"]=self.lineEditRemotePathDeb.text()
+        tools["filterDeb"]=self.lineEditFilterDeb.text()
+
+        
+        # check root
         if os.geteuid() != 0:
             with open('/tmp/tools.yaml', 'w') as object_file:
                 yaml.dump(tools, object_file)
@@ -63,3 +74,9 @@ class Config_Tools(QtWidgets.QDialog, Ui_Dialog):
         else:
             with open(self.tools_yaml_path, 'w') as object_file:
                 yaml.dump(tools, object_file)
+
+
+
+    @QtCore.Slot()
+    def close_me(self):
+        self.close()
