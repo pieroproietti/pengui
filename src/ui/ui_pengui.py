29cd9d06ce541fb8af35e9b5eb9147bc03841de4
Piero Proietti <piero.proietti@gmail.com>
2024-04-01 17:05:33 +0200

    pulite le icone occorrono nuove

--- src/ui/ui_pengui.py
+++ src/ui/ui_pengui.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'penguiYPoOpz.ui'
+## Form generated from reading UI file 'penguiCDRyud.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.6.3
 ##
